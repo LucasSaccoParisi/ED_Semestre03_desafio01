388e7221546651c8a8214fd5532fc710ccae118c
Lucas Sacco Parisi <lucas.sparisi@senacsp.edu.br>
2026-05-13 08:42:59 -0300

    Add files via upload

---
+++ aula06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Senac.ED.BST;
+
+public class Program {
+    public static void Main(string[] args) {
+        BST bst = new BST();
+        
+        bst.Insert(15);
+        bst.Insert(10);
+        bst.Insert(8);
+        bst.Insert(12);
+        bst.Insert(20);
+        bst.Insert(21);
+        
+        Console.WriteLine("In-order traversal (Sorted keys):");
+        bst.PrintInOrder();
+        Console.WriteLine("Visualização mais legal:");
+        bst.PrintNicely();
+    }
+}
